b415c3fe6adc3e3a8613b5810666acebfdc89dce
davednguyen <davednguyen@gmail.com>
2018-04-19 19:02:53 -0700

    update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function countCookie() {
     color += letters[Math.floor(Math.random() * letters.length)];
     document.getElementById("counter").innerHTML = count;
     if (count % 10 == 0) {
-        document.getElementById("note").innerHTML = "congrate you just gained 1 lb to your love body!!! :)";
+        document.getElementById("note").innerHTML = "congrate you just gained 1 lb to your lovely body!!! :)";
         ran_col();
     }
 }
