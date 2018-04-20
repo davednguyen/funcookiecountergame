050c2f3240c1c21ff5c3f248a66f137183928bef
davednguyen <davednguyen@gmail.com>
2018-04-19 20:28:06 -0700

    removed black background color

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function clickHandler() {
 
 function ran_col() { //function name
     let color = '#'; // hexadecimal starting symbol
-    const letters = ['000000', 'FF0000', '00FF00', '0000FF', 'FFFF00', '00FFFF', 'FF00FF', 'C0C0C0']; //Set your colors here
+    const letters = ['FF0000', '00FF00', '0000FF', 'FFFF00', '00FFFF', 'FF00FF', 'C0C0C0']; //Set your colors here
     color += letters[Math.floor(Math.random() * letters.length)];
     document.getElementById('note').style.background = color; // Setting the random color on your div element.
 }
