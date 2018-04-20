1e78371aa7651ed2536ad5e413a032e77ac69ef2
davednguyen <davednguyen@gmail.com>
2018-04-19 19:12:13 -0700

    update

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-var count = 0;
-var color = '#'; // hexadecimal starting symbol
-var letters = ['000000', 'FF0000', '00FF00', '0000FF', 'FFFF00', '00FFFF', 'FF00FF', 'C0C0C0']; //Set your colors here
+let count = 0;
+let color = '#'; // hexadecimal starting symbol
+const letters = ['000000', 'FF0000', '00FF00', '0000FF', 'FFFF00', '00FFFF', 'FF00FF', 'C0C0C0']; //Set your colors here
 
 function countCookie() {
     count++;
     color += letters[Math.floor(Math.random() * letters.length)];
     document.getElementById("counter").innerHTML = count;
     if (count % 10 == 0) {
-        document.getElementById("note").innerHTML = "congrate you just gained 1 lb to your Lovely body!!! :)";
+        document.getElementById("note").innerHTML = "congrats you just gained 1 lb to your Lovely body!!! :)";
         ran_col();
     }
 }
 
-var img = document.querySelector("img#clickable");
+let img = document.querySelector("img#clickable");
 img.addEventListener("click", clickHandler);
 
-var counter = 0;
+let counter = 0;
 
 function clickHandler() {
     console.log("clickHandler called");
@@ -25,8 +25,28 @@ function clickHandler() {
 }
 
 function ran_col() { //function name
-    var color = '#'; // hexadecimal starting symbol
-    var letters = ['000000', 'FF0000', '00FF00', '0000FF', 'FFFF00', '00FFFF', 'FF00FF', 'C0C0C0']; //Set your colors here
+    let color = '#'; // hexadecimal starting symbol
+    const letters = ['000000', 'FF0000', '00FF00', '0000FF', 'FFFF00', '00FFFF', 'FF00FF', 'C0C0C0']; //Set your colors here
     color += letters[Math.floor(Math.random() * letters.length)];
     document.getElementById('note').style.background = color; // Setting the random color on your div element.
 }
+
+const trees = ["oak", "elm", "lemon"];
+let counter2 = 0;
+const maxCount = 3;
+
+function playGuessingGame(guess) {
+    counter2 += 1;
+    if (trees.indexOf(guess) >= 0) {
+        alert("go got it!");
+        return;
+    }
+    if (counter2 < maxCount) {
+        playGuessingGame(prompt("guess a tree!"));
+    } else {
+        alert("sorry, out of gueeses :(");
+        return;
+    }
+}
+
+playGuessingGame(prompt("guess a tree"));
